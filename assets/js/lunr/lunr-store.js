af8a91407c5e8e7dc004bdbf2e75fbd0525ff2f1
MihajloNesic <MihajloNesic@users.noreply.github.com>
2022-12-08 09:15:19 +0000

    jekyll build from Action dac0760320e5b858058e5ffaf8505fa50a5f22c1

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll-pdf-embed-starter/welcome-to-jekyll",
+        "teaser": null
+      },{
+        "title": "jekyll-pdf-embed test",
+        "excerpt":"{% pdf \"https://mihajlonesic.gitlab.io/files/loremipsum.pdf\" %} View PDF Base URL must be present if baseurl is set in _config.yml. In this example, minimal-mistakes-pdf-example is prepended to file location /files/dummy.pdf. {% pdf \"/jekyll-pdf-embed-starter/files/dummy.pdf\" no_link height=300px %} Another example is using file from front matter --- pdf_file: \"/jekyll-pdf-embed-starter/files/dummy.pdf\" --- {% pdf {{ page.pdf_file }}...","categories": ["jekyll","plugin"],
+        "tags": [],
+        "url": "/jekyll-pdf-embed-starter/jekyll-pdf-embed-test",
+        "teaser": null
+      }]
